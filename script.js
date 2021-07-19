fbc0614e6bf8081a290ef4610491ee25dd2b3b65
Andras Huber <kiskelcsi@gmail.com>
2021-07-19 18:51:23 +0200

    07.19

--- script.js
+++ script.js
@@ -40,4 +40,90 @@ function scrollToSection(e) {
   console.log(elmnt);
 }
 
-// GALERY (MODAL)
+// ANIMATED BOX
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.addEventListener("scroll", function (event) {
+    const animatedBoxes = document.getElementsByClassName("animated-box");
+    const windowOffsetTop = window.innerHeight + window.scrollY;
+
+    Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+      const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+      if (windowOffsetTop >= animatedBoxOffsetTop) {
+        addClass(animatedBox, "fade-in");
+      }
+    });
+  });
+});
+
+function addClass(element, className) {
+  const arrayClasses = element.className.split(" ");
+  if (arrayClasses.indexOf(className) === -1) {
+    element.className += " " + className;
+  }
+}
+
+// MODAL BOX
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
