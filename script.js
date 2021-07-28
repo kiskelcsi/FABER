5d255ac275226e0af2e897e5c988cf6a879ce827
Andras Huber <kiskelcsi@gmail.com>
2021-07-28 07:35:52 +0200

    scroll ios

--- script.js
+++ script.js
@@ -208,21 +208,45 @@ window.addEventListener("load", function () {
   });
 });
 
-// SECTION FRIENDS
+// SCROLL
 
-const friendsSlide = document.querySelectorAll(".friends_col");
+(function () {
+  scrollTo();
+})();
 
-const flexCont = function (slide) {
-  // friendsSlide.forEach((s) => (s.style.flex = "0.4"));
-  // document
-  //   .querySelectorAll(".friends_col")
-  //   .forEach((dot) => dot.classList.remove("dots__dot--active"));
-};
-
-// let idOfBox = document.addEventListener("mouseover", function (e) {
-//   const flexI = e.target.id;
-
-//   document
-//     .querySelectorAll(".friends_col")
-//     .forEach((dot) => dot.classList.add("sizemore"));
-// });
+function scrollTo() {
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    if (
+      link.href &&
+      link.href.indexOf("#") != -1 &&
+      (link.pathname == location.pathname ||
+        "/" + link.pathname == location.pathname) &&
+      link.search == location.search
+    ) {
+      link.onclick = scrollAnchors;
+    }
+  }
+}
+function scrollAnchors(e, respond = null) {
+  const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
+  e.preventDefault();
+  var targetID = respond
+    ? respond.getAttribute("href")
+    : this.getAttribute("href");
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+  const checkIfDone = setInterval(function () {
+    const atBottom =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = "-1";
+      targetAnchor.focus();
+      window.history.pushState("", "", targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
+}
